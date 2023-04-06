1794e4ca48e72e893c2b3752202c91ac545a9810
Chitranshoo Prakash <chitranshoo.prakash@jarvis.consulting>
2023-04-06 09:44:42 +0530

    authentication completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {RestService} from "./rest.service";
-import {distinct} from "rxjs";
 import {AuthService} from "./auth/auth.service";
 
 @Component({
--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -1,8 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
-import {exhaustMap, Subject, take} from "rxjs";
 import {AuthService} from "./auth/auth.service";
-import {User} from "./auth/user.model";
 
 @Injectable({
   providedIn: 'root'
